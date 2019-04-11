7550367ba4e620ce96606e8c1fa2c93ea466dbc8
Francis Chen <fncis.chen@gmail.com>
2019-04-11 12:53:13 -0700

    refactored display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,17 +3,20 @@ import Link from '../Link.react';
 import renderer from 'react-test-renderer';
 import Display from "./Display";
 
+describe("Display", () => { 
 
-it('If Display Gate is locked', () => {
-  const tree = renderer
-    .create(<Display locked={true} closed={true}/>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-it('If Display Gate is unlocked', () => {
+    it('If Display Gate is locked', () => {
     const tree = renderer
-      .create(<Display locked={false} closed={false}/>)
-      .toJSON();
+        .create(<Display locked={true} closed={true}/>)
+        .toJSON();
     expect(tree).toMatchSnapshot();
-  });
+    });
+
+    it('If Display Gate is unlocked', () => {
+        const tree = renderer
+        .create(<Display locked={false} closed={false}/>)
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
