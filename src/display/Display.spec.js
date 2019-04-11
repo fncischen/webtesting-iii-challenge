1af42fb3f599ab3c568bdda69def4f437ff62e76
Francis Chen <fncis.chen@gmail.com>
2019-04-11 13:04:32 -0700

    added snapshot to match for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,16 +7,56 @@ describe("Display", () => {
 
     it('If Display Gate is locked', () => {
     const tree = renderer
-        .create(<Display locked={true} closed={true}/>)
+        .create(<Display locked={true}/>)
         .toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot(
+        <div className="display panel">
+        <div className={lockedClass}>Locked</div>
+        <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+      </div>
+    );
     });
 
     it('If Display Gate is unlocked', () => {
         const tree = renderer
-        .create(<Display locked={false} closed={false}/>)
+        .create(<Display locked={false}/>)
         .toJSON();
-        expect(tree).toMatchSnapshot();
+        expect(tree).toMatchSnapshot(
+
+            <div className="display panel">
+            <div className={lockedClass}>Unlocked</div>
+            <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+          </div>
+
+        );
+    });
+
+    it('If Display Gate is closed', () => {
+        const tree = renderer
+            .create(<Display closed={true}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot(
+
+            <div className="display panel">
+            <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
+            <div className={closedClass}>Closed</div>
+            </div>
+
+        );
+        });
+    
+    it('If Display Gate is open', () => {
+            const tree = renderer
+            .create(<Display closed={false}/>)
+            .toJSON();
+            expect(tree).toMatchSnapshot(
+
+                <div className="display panel">
+                <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
+                <div className={closedClass}>Open</div>
+                </div>
+
+            );
     });
 
 });
