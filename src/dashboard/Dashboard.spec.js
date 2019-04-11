c5999cec9c252db27e8efc3aa1757fa33be685f0
Francis Chen <fncis.chen@gmail.com>
2019-04-11 12:44:31 -0700

    set up Snapshot tests for Controls, Dashboard, and Display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+import React from 'react';
+import Link from '../Link.react';
+import renderer from 'react-test-renderer';
+
+import Display from "../display/display.js"
+import Controls from "../controls/controls.js"
+
+it('Display is rendering', () => {
+  const tree = renderer
+    .create(<Display/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Controls is rendering', () => {
+    const tree = renderer
+      .create(<Controls/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react';
+import Link from '../Link.react';
+import renderer from 'react-test-renderer';
+import Display from "./Display";
+
+
+it('If Display Gate is locked', () => {
+  const tree = renderer
+    .create(<Display locked={true} closed={true}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('If Display Gate is unlocked', () => {
+    const tree = renderer
+      .create(<Display locked={false} closed={false}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
